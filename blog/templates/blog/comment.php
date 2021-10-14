7636b160b051bce6d820aa39ce980d229d3447fb
Your Name <you@example.com>
2021-10-14 23:38:19 +0900

    Changed

--- blog/templates/blog/comment.php
+++ blog/templates/blog/comment.php
@@ -1,7 +1,7 @@
 <?php
 
 /* 変数の定義 */////////////////////////////////////////////////////////////////////
-  $setchar='Shift_JIS';/* 日本語文字セット UTF-8, Shift_JIS, EUC-JP,ISO-2022-JP */
+  $setchar='UTF-8';/* 日本語文字セット Shift_JIS, Shift_JIS, EUC-JP,ISO-2022-JP */
   $pdate=$date=date("<b>Y/m/d H:i </b>");/* 日時自動記入するとき設定 */
   $lmt=100;/* 投稿文字数制限 */
   $kugiri='<hr style="border:1px solid #eee">';/* 記事の区切り */
@@ -36,7 +36,7 @@
   if($_GET['rep']&&!$_POST['comment'])$comment="";
   header("cache-control: no-store, no-cache, must-revalidate");
   header("cache-control: post-check=0, pre-check=0");
-  header("content-type: text/html; charset=Shift_JIS");
+  header("content-type: text/html; charset=UTF-8");
   if($comment){$comment=preg_replace("/&amp;nbsp;/","&nbsp;",$comment);
     $comment=preg_replace("/&lt;br&gt;/i","<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;",$comment);
     $comment=preg_replace("/&lt;b&gt;/i","<b>",$comment);
